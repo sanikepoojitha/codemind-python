0078de3fb6d8364182e4b63ed632cea983c9619d
sanikepoojitha <97931090+sanikepoojitha@users.noreply.github.com>
2022-06-27 21:31:35 +0530

    n=int(input()) l=list(map(int,input().split())) c=0 f=0 for i in range(n):     if l[i]%2==0:         if(i%2!=0):             c+=1
    if c==0:
        print("True")
    else:
        print("False")

---
+++ Strictly_EVEN.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+f=0
+for i in range(n):
+    if l[i]%2==0:
+        if(i%2!=0):
+            c+=1
+            
+if c==0:
+    print("True")
+else:
+    print("False")
