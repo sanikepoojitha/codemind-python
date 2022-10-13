6877b27f03558c926bd8afa532ba2ae134b1f48c
sanikepoojitha <97931090+sanikepoojitha@users.noreply.github.com>
2022-10-13 21:01:59 +0530

    n=int(input()) l=list(map(int,input().split())) k=sum(l)//n for i in range(n):     if l[i]==k:         print("True")         break else:     print("False")

--- average_element_is_in_array_or_not.py
+++ average_element_is_in_array_or_not.py
@@ -1,14 +1,9 @@
 n=int(input())
 l=list(map(int,input().split()))
-s=0
-c=0
+k=sum(l)//n
 for i in range(n):
-    s+=l[i]
-a=s//n
-for i in range(n):
-    if l[i]==a:
-        c+=1
-if c==0:
-    print("False")
+    if l[i]==k:
+        print("True")
+        break
 else:
-    print("True")
+    print("False")
