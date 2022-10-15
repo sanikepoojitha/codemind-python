843518001376d72afbb1d2ce22b7ac42e248bdef
sanikepoojitha <97931090+sanikepoojitha@users.noreply.github.com>
2022-10-15 22:39:35 +0530

    n=int(input()) l=list(map(int,input().split())) a=[] b=0 for i in range(n):     c=0     for j in range(n):         if l[i]==l[j]:             c+=1     if c==l[i]:         b+=1         l[i]=0 print(b)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,12 +1,13 @@
 n=int(input())
 l=list(map(int,input().split()))
-max=0
+a=[]
+b=0
 for i in range(n):
     c=0
     for j in range(n):
         if l[i]==l[j]:
             c+=1
     if c==l[i]:
-        max+=1
+        b+=1
         l[i]=0
-print(max)
+print(b)
