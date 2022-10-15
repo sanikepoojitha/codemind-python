f1fa64a3aea8d0a5d557b0c717812bdc38df1cff
sanikepoojitha <97931090+sanikepoojitha@users.noreply.github.com>
2022-10-15 22:53:38 +0530

    n=int(input()) l=list(map(int,input().split())) l=set(l) c=0 l=list(l) for i in l:     c+=i print(c)

--- unique_sum.py
+++ unique_sum.py
@@ -1,11 +1,8 @@
 n=int(input())
 l=list(map(int,input().split()))
-s=0
-for i in range(n):
-    c=0
-    for j in range(i,n):
-        if l[i]==l[j]:
-            c+=1
-    if c==1:
-        s=s+l[i]
-print(s)
+l=set(l)
+c=0
+l=list(l)
+for i in l:
+    c+=i
+print(c)
