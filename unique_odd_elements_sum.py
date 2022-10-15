fc2f4a171bef49859b32d3b1e054a8332f4189cd
sanikepoojitha <97931090+sanikepoojitha@users.noreply.github.com>
2022-10-15 23:06:39 +0530

    n=int(input()) l=list(map(int,input().split())) l=set(l) c=0 l=list(l) for i in l:     if i%2!=0:         c+=i print(c)

--- unique_odd_elements_sum.py
+++ unique_odd_elements_sum.py
@@ -1,13 +1,9 @@
 n=int(input())
 l=list(map(int,input().split()))
-s=0
-for i in range(n):
-    c=0
-    for j in range(i,n):
-        if l[i]==l[j]:
-            c+=1
-    if c==1:
-        if l[i]%2==1:
-            s+=l[i]
-        
-print(s) 
+l=set(l)
+c=0
+l=list(l)
+for i in l:
+    if i%2!=0:
+        c+=i
+print(c)
