3434452df03f2e7a97c4419639af6d413026ee52
sanikepoojitha <97931090+sanikepoojitha@users.noreply.github.com>
2022-10-15 22:35:07 +0530

    n=int(input()) l=list(map(int,input().split())) a=[] min=999 max=0 c=0 for i in range(n):     h=0     for j in range(n):         if l[i]==l[j]:             h+=1     if h==l[i]:         if l[i]<min:             min=l[i]         if l[i]>max:             max=l[i]         c+=1         l[i]=0 a.append(min) a.append(max) if c==0:     print("-1") else:     print(*a)

--- Maximum_and_Minimum.py
+++ Maximum_and_Minimum.py
@@ -1,20 +1,24 @@
 n=int(input())
 l=list(map(int,input().split()))
-min=99999999
+a=[]
+min=999
 max=0
-g=0
+c=0
 for i in range(n):
-    c=0
+    h=0
     for j in range(n):
         if l[i]==l[j]:
-            c+=1
-    if c==l[i]:
-        if l[i]>max:
-            max=l[i]
+            h+=1
+    if h==l[i]:
         if l[i]<min:
             min=l[i]
-        g+=1
-if g==0:
-    print('-1')
+        if l[i]>max:
+            max=l[i]
+        c+=1
+        l[i]=0
+a.append(min)
+a.append(max)
+if c==0:
+    print("-1")
 else:
-    print(min,max)
+    print(*a)
