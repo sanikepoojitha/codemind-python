e8362e45904ae9c572904df351277e9f0ba6f353
sanikepoojitha <97931090+sanikepoojitha@users.noreply.github.com>
2022-10-16 10:13:42 +0530

    l=list(map(str,input().split())) for i in range(len(l)):     if i%2==0:         l[i]=l[i][::-1] print(*l)

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -1,13 +1,5 @@
-def rev(s):
-    s1=""
-    l=len(s)
-    for i in range(l-1,-1,-1):
-        s1+=s[i]
-    return s1
-a=input()
-arr=list(a.split())
-for i in range(len(arr)):
+l=list(map(str,input().split()))
+for i in range(len(l)):
     if i%2==0:
-        print(rev(arr[i]),end=" ")
-    else:
-        print(arr[i],end=" ")
+        l[i]=l[i][::-1]
+print(*l)
