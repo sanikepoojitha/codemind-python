abf29f7df5357c9730a8b148210daf3bc45e8cc9
sanikepoojitha <97931090+sanikepoojitha@users.noreply.github.com>
2023-01-12 11:28:45 +0530

    l=list(map(str,input().split())) min=999 for i in range(len(l)):     if len(l[i])<min:         min=len(l[i]) print(min)

--- minimum_length_of_words.py
+++ minimum_length_of_words.py
@@ -1,10 +1,6 @@
-a=input()
-arr=list(a.split())
-mi=100
-for i in arr:
-    c=0
-    for  j in i:
-        c+=1
-    if c<mi:
-        mi=c
-print(mi)
+l=list(map(str,input().split()))
+min=999
+for i in range(len(l)):
+    if len(l[i])<min:
+        min=len(l[i])
+print(min)
