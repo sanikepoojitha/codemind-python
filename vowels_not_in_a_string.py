141204b643339e0255909f7689e134c9ee2a6329
sanikepoojitha <97931090+sanikepoojitha@users.noreply.github.com>
2023-01-12 11:20:37 +0530

    s='aeiou' n=input() a=[] for i in s:     c=0     for j in n:         if i==j:             c+=1     if c==0:         a.append(i) if len(a)>0:     print(*a) else:     print('0')

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -1,9 +1,14 @@
-a=input()
-vo="aeiou"
-c=0
-for i in vo:
-    if i not in a:
-        print(i,end=" ")
-        c+=1;
-if(c==0):
-    print(0)
+s='aeiou'
+n=input()
+a=[]
+for i in s:
+    c=0
+    for j in n:
+        if i==j:
+            c+=1
+    if c==0:
+        a.append(i)
+if len(a)>0:
+    print(*a)
+else:
+    print('0')
