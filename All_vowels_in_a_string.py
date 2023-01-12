c62edfa4a530256542a62f60437f3512cfd8cbef
sanikepoojitha <97931090+sanikepoojitha@users.noreply.github.com>
2023-01-12 11:26:58 +0530

    n=input() aa=ee=ii=oo=uu=0 for p in n:     if p=='a':         aa+=1     elif p=='e':         ee+=1     elif p=='o':         oo+=1     elif p=='i':         ii+=1     elif p=='u':         uu+=1 if aa==0 or ee==0 or ii==0 or oo==0 or uu==0:     print("False") else:     print("True")

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,13 +1,17 @@
-a=input()
-f=0
-vo="aeiou"
-for i in vo:
-    if i in a:
-        f=1
-    else:
-        f=0
-        break
-if f==1:
-    print("True")
-else:
+n=input()
+aa=ee=ii=oo=uu=0
+for p in n:
+    if p=='a':
+        aa+=1
+    elif p=='e':
+        ee+=1
+    elif p=='o':
+        oo+=1
+    elif p=='i':
+        ii+=1
+    elif p=='u':
+        uu+=1
+if aa==0 or ee==0 or ii==0 or oo==0 or uu==0:
     print("False")
+else:
+    print("True")
